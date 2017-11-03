bfa0e085b6a9e1d3e8acbc96ab07d07a38225207
leieo <leila.el.omari@gmail.com>
2017-11-03 15:12:52 +0100

    create a view - separate data access code

--- index.php
+++ index.php
@@ -1,55 +1,26 @@
-<!DOCTYPE html>
+<?php
 
-<html>
+// variables qui deviendront les paramètres de la base de données :
+$dataSourceName = 'mysql:host=localhost;dbname=microcms;charset=utf8';
+$user = 'root';
+$password = 'simplonco';
 
-<head>
-    <meta charset="utf-8" />
-    <link href="microcms.css" rel="stylesheet" />
-    <title>MicroCMS - Home</title>
-</head>
 
-<body>
-    <header>
-        <h1>MicroCMS</h1>
-    </header>
+// connexion à la base de donnée et message d'erreur :
+try {
+    $database = new PDO($dataSourceName, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+} 
 
-    <?php
-
-    $dataSourceName = 'mysql:host=localhost;dbname=microcms;charset=utf8';
-    $user = 'root';
-    $password = 'simplonco';
+// récupération des données qui nous intéressent :
+$posts = $database->query('SELECT * FROM t_posts order by post_id desc'); 
 
 
 
-    try {
-        $database = new PDO($dataSourceName, $user, $password);
-    } catch (PDOException $e) {
-        echo 'Connexion échouée : ' . $e->getMessage();
-    } 
+// on inclut et exécute le fichier view.php :
+require 'view.php';
 
-    $posts = $database->query('SELECT * FROM t_posts order by post_id desc');
 
-    foreach ($posts as $post): 
-
-    ?>
-
-    <article>
-        <h2><?php echo $post['post_title'] ?></h2>
-        <p><?php echo $post['post_content'] ?></p>
-    </article>
-
-    <?php 
-
-    endforeach 
-
-    ?>
-
-    <footer class="footer">
-
-        <a href="https://github.com/bpesquet/OC-MicroCMS">MicroCMS</a> is a minimalistic CMS built as a showcase for modern PHP development.
-
-    </footer>
-
-</body>
-
-</html>
+// ommission volontaire de la balise fermante :
+// bonne pratique pour les fichiers ne contenant que du php
---
+++ view.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <link href="microcms.css" rel="stylesheet" />
+        <title>MicroCMS - Home</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>MicroCMS</h1>
+        </header>
+
+        <?php foreach ($posts as $post):  ?>
+
+        <article>
+            <h2><?php echo $post ['post_title'] ?></h2>
+            <p><?php echo $post['post_content'] ?></p>
+        </article>
+
+        <?php 
+
+        endforeach 
+
+        ?>
+
+        <footer class="footer">
+
+            <a href="https://github.com/bpesquet/OC-MicroCMS">MicroCMS</a> is a minimalistic CMS built as a showcase for modern PHP development
+
+        </footer>
+
+    </body>
+
+</html>
