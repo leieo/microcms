2ed6801861cde2b83ae6c9c81d751595ae732ce6
leieo <leila.el.omari@gmail.com>
2017-11-09 14:10:00 +0100

    create create-view

---
+++ create-view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link href="microcms.css" rel="stylesheet" />
+  </head>
+  <body>
+  	<h2><?php echo $_POST['titre']; ?></h2>
+  	<p><?php echo $_POST['contenu']; ?></p>
+  	<br/><br/><br/>
+  	<p>Cet article a été ajouté - <a href="index.php">Retour au journal</a></p>
+  </body>
+</html>
+
--- create.php
+++ create.php
@@ -1,23 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <link href="microcms.css" rel="stylesheet" />
-  </head>
-  <body>
-
     <?php
 
     require 'model.php';
 
     createPost();
 
-    ?>
-
-	<h2><?php echo $_POST['titre']; ?></h2>
-	<p><?php echo $_POST['contenu']; ?></p>
-	<br/><br/><br/>
-	<p>Cet article a été ajouté - <a href="index.php">Retour au journal</a></p>
-  </body>
-</html>
-
+    require 'create-view.php';
